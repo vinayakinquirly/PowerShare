ae6f071fbc5dae8ac0bf24ccbb37eb3a2681cef0
Vinayak <vinayak@inquirly.com>
2016-08-19 15:49:09 +0530

    gradle dependencies

--- app/src/main/java/com/inquirly/inquirlyforbusinesses/activity/MainActivity.java
+++ app/src/main/java/com/inquirly/inquirlyforbusinesses/activity/MainActivity.java
@@ -1,9 +1,8 @@
 package com.inquirly.inquirlyforbusinesses.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.inquirly.inquirlyforbusinesses.R;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
