b8862309957152a63c43236cd3547e59a00038c4
Vinayak <vinayak@inquirly.com>
2016-08-22 10:47:19 +0530

    previous left data merging on MONDAY

---
+++ app/src/main/java/com/inquirly/inquirlyforbusinesses/activity/OnBoardingActivity.java
@@ -0,0 +1,15 @@
+package com.inquirly.inquirlyforbusinesses.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.inquirly.inquirlyforbusinesses.R;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
